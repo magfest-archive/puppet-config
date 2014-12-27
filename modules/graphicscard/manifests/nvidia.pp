52dfcb64549ddddd776aa9822bf52b877ed790ee
Mark Murnane <mark25@umbc.edu>
2014-12-27 18:44:17 -0500

    Added configuration for nvidia drivers

--- modules/graphicscard/manifests/nvidia.pp
+++ modules/graphicscard/manifests/nvidia.pp
@@ -1,23 +1,5 @@
 class graphicscard::nvidia {
-  package { 'xorg-x11-drv-nvidia-319.76':
+  package { 'xf86-video-nouveau':
     ensure => latest
-  }
-  package { 'xorg-x11-drv-nvidia-libs-319.76':
-    ensure => latest
-  }
-  package { 'akmod-nvidia-319.76':
-    ensure => latest
-  }
-  file { '/etc/X11/xorg.conf':
-    source => 'puppet:///modules/graphicscard/xorg.conf'
-  }
-  file { '/etc/X11/xorg.conf.d/00-keyboard.conf':
-    source => 'puppet:///modules/graphicscard/00-keyboard.conf'
-  }
-  file { '/etc/X11/xorg.conf.d/00-nvidia.conf':
-    source => 'puppet:///modules/graphicscard/00-nvidia.conf'
-  }
-  file { '/etc/X11/xorg.conf.d/10-libvnc.conf':
-    source => 'puppet:///modules/graphicscard/10-libvnc.conf'
   }
 }
