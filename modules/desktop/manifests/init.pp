de6cdc896bedc0c6fcdd2798be14bbb2ae2736c4
Mark Murnane <mark25@umbc.edu>
2014-12-27 12:19:39 -0500

    Installing gtk3 theme for lightdm

--- modules/desktop/manifests/init.pp
+++ modules/desktop/manifests/init.pp
@@ -1,21 +1,24 @@
 class desktop {
   package { 'mate':
     ensure => present
-  }
+  } ->
   package { 'lightdm':
     ensure => latest
-  }
+  } ->
+  package { 'xorg-server':
+    ensure => latest
+  } ->
+  package { 'xorg-server-utils':
+    ensure => latest
+  } ->
+  package { 'xorg-xinit':
+    ensure => latest
+  } ->
+  package { 'lightdm-gtk3-greeter':
+    ensure => latest
+  } ->
   service { 'lightdm':
     ensure => running,
     enable => true
-  }
-  package { 'xorg-server':
-    ensure => latest
-  }
-  package { 'xorg-server-utils':
-    ensure => latest
-  }
-  package { 'xorg-xinit':
-    ensure => latest
   }
 }
