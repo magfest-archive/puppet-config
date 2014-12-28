50cce755e4952fb99eb7801049a0918f391ee1a3
Mark Murnane <mark25@umbc.edu>
2014-12-28 16:12:19 -0500

    Adding directory for anitmicro

--- modules/emulators/manifests/init.pp
+++ modules/emulators/manifests/init.pp
@@ -1,4 +1,7 @@
 class emulators {
+  file { ['/root', '/root/.config', '/root/.config/antimicro']:
+    ensure => directory
+  }
   file { '/root/.config/antimicro/antimicro_settings.ini':
     ensure => file,
     source => 'puppet:///modules/emulators/antimicro_settings.ini',
