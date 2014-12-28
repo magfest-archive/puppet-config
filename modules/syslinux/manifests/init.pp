b2fc50bd1404668c3bee70c1664ec3ae762784f7
Mark Murnane <mark25@umbc.edu>
2014-12-28 16:02:50 -0500

    Added syslinux config to handle boot password

--- modules/syslinux/manifests/init.pp
+++ modules/syslinux/manifests/init.pp
@@ -1,6 +1,26 @@
 class syslinux {
-  file { '/boot/syslinux/syslinux.cfg':
-    ensure => file,
-    source => 'puppet:///modules/syslinux/syslinux.cfg'
+  file { '/boot/syslinux/challenge.cfg':
+    source => 'puppet:///modules/syslinux/challenge.cfg'
+  }
+  file { '/boot/syslinux/registration.cfg':
+    source => 'puppet:///modules/syslinux/registration.cfg'
+  }
+  file { '/boot/syslinux/kiosk.cfg':
+    source => 'puppet:///modules/syslinux/kiosk.cfg'
+  }
+  file { '/etc/systemd/system/challenge.target':
+    source => 'puppet:///modules/syslinux/challenge.target'
+  }
+  file { '/etc/systemd/system/setboot-challenges.service':
+    source => 'puppet:///modules/syslinux/setboot-challenges.service'
+  }
+  file { '/etc/systemd/system/setboot-reg.service':
+    source => 'puppet:///modules/syslinux/setboot-reg.service'
+  }
+  service { 'setboot-challenges':
+    enable => true
+  }
+  service { 'setboot-reg':
+    enable => true
   }
 }
