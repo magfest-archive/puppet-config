8af81d7739236d027e9e51c4c44762565ee5f2b5
Mark Murnane <mark25@umbc.edu>
2014-12-28 16:04:59 -0500

    Fixed typo and ordering issue

--- modules/syslinux/manifests/init.pp
+++ modules/syslinux/manifests/init.pp
@@ -1,25 +1,25 @@
 class syslinux {
-  file { '/boot/syslinux/challenge.cfg':
-    source => 'puppet:///modules/syslinux/challenge.cfg'
-  }
+  file { '/boot/syslinux/challenges.cfg':
+    source => 'puppet:///modules/syslinux/challenges.cfg'
+  } ->
   file { '/boot/syslinux/registration.cfg':
     source => 'puppet:///modules/syslinux/registration.cfg'
-  }
+  } ->
   file { '/boot/syslinux/kiosk.cfg':
     source => 'puppet:///modules/syslinux/kiosk.cfg'
-  }
+  } ->
   file { '/etc/systemd/system/challenge.target':
     source => 'puppet:///modules/syslinux/challenge.target'
-  }
+  } ->
   file { '/etc/systemd/system/setboot-challenges.service':
     source => 'puppet:///modules/syslinux/setboot-challenges.service'
-  }
+  } ->
   file { '/etc/systemd/system/setboot-reg.service':
     source => 'puppet:///modules/syslinux/setboot-reg.service'
-  }
+  } ->
   service { 'setboot-challenges':
     enable => true
-  }
+  } ->
   service { 'setboot-reg':
     enable => true
   }
