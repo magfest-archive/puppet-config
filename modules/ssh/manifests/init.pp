5e3cae3ac5b0beaf0a53107b6a374dcea7c45a18
Mark Murnane <mark25@umbc.edu>
2014-12-27 23:38:46 -0500

    Fixed ssh config to simplify it

--- modules/ssh/manifests/init.pp
+++ modules/ssh/manifests/init.pp
@@ -1,15 +1,17 @@
-class ssh ($allowgroups = '', $allowusers = ["root"], $authkeys = ['mark']) {
+class ssh ($authkeys = ['mark']) {
   ssh::key { $authkeys:
     user => 'root'
   }
   package { 'openssh':
     ensure => latest
-  } ->
+  }
   service { 'sshd':
     enable => true
   }
-  class { 'ssh::config':
-    allowgroups => $allowgroups,
-    allowusers => $allowusers
+  file { '/etc/ssh/sshd_config':
+    source => 'puppet:///modules/ssh/sshd_config'
+  }
+  file { /etc/issue':
+    source => 'puppet:///modules/ssh/issue'
   }
 }
