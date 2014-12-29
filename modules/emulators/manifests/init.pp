3d551ded73e521289f16d4a9e20e1a2e7c4d1d5c
Mark Murnane <mark25@umbc.edu>
2014-12-29 15:20:49 -0500

    Added an attempt at challenges configuration

--- modules/emulators/manifests/init.pp
+++ modules/emulators/manifests/init.pp
@@ -24,9 +24,25 @@ class emulators {
   exec { '/usr/bin/pacman --noconfirm -U http://192.168.5.1/bootmedia/install/pkgs/evrouter-0.4-2-i686.pkg.tar.xz':
     creates => '/usr/bin/evrouter'
   }
+  user { 'challenges':
+    ensure => present,
+    groups => 'autologin'
+  }
+  file { '/home/challenges':
+    ensure => directory,
+    owner => challenges,
+    group => challenges
+  }
+  file { '/usr/bin/startchallenges':
+    source => 'puppet:///modules/emulators/startchallenges',
+    mode => 755
+  } ->
   file { '/etc/systemd/system/challenge.service':
     source => 'puppet:///modules/emulators/challenge.service'
-  }
+  } ->
+  file { '/etc/lightdm/challenges.conf':
+    source => 'puppet:///modules/emulators/challenges.conf'
+  } ->
   service { 'challenge':
     enable => true,
     require => File['/etc/systemd/system/challenge.service']
