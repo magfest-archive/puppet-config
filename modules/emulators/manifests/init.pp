eeacc9b0ee283caaf8359a0ad051386438b83ad4
Mark Murnane <mark25@umbc.edu>
2014-12-29 13:55:50 -0500

    Fixed paths

--- modules/emulators/manifests/init.pp
+++ modules/emulators/manifests/init.pp
@@ -1,5 +1,5 @@
 class emulators {
-  exec { '/usr/bin/pacman --noconfirm -U http://192.168.5.1/bootmedia/install/challenges-13.0.0.1-1-any.pkg.tar.xz':
+  exec { '/usr/bin/pacman --noconfirm -U http://192.168.5.1/bootmedia/install/pkgs/challenges-13.0.0.1-1-any.pkg.tar.xz':
     creates => '/home/magfest/.fceux/fceux.cfg'
   }
   file { ['/root', '/root/.config', '/root/.config/antimicro']:
@@ -21,7 +21,7 @@ class emulators {
   package { 'gens-gs':
     ensure => latest
   }
-  exec { '/usr/bin/pacman --noconfirm -U http://192.168.5.1/bootmedia/install/evrouter-0.4-2-i686.pkg.tar.xz':
+  exec { '/usr/bin/pacman --noconfirm -U http://192.168.5.1/bootmedia/install/pkgs/evrouter-0.4-2-i686.pkg.tar.xz':
     creates => '/usr/bin/evrouter'
   }
 }
