0a7430b758c5b520d722a1fbb1b52a5a5050a4c9
Mark Murnane <mark25@umbc.edu>
2015-01-22 21:31:33 -0500

    Added rules to install challenges

--- modules/emulators/manifests/init.pp
+++ modules/emulators/manifests/init.pp
@@ -1,19 +1,21 @@
 class emulators {
-  exec { '/usr/bin/pacman --noconfirm -U http://foreman.magfest.net/bootmedia/install/pkgs/challenges-13.0.0.4-1-any.pkg.tar.xz':
-    creates => "/opt/challenges"
-  }
+#  exec { '/usr/bin/pacman --noconfirm -U http://foreman.magfest.net/bootmedia/install/pkgs/challenges-13.0.0.4-1-any.pkg.tar.xz':
+#    creates => "/opt/challenges"
+#  }
   package { 'ttf-freefont':
     ensure => present
   }
-#  exec { '/usr/bin/pacman --noconfirm -U http://foreman.magfest.net/bootmedia/install/pkgs/antimicro-2.6-1-x86_64.pkg.tar.xz':
-#    creates => "/opt/antimicro"
-#  }
-  file { ['/root', '/root/.config', '/root/.config/antimicro']:
-    ensure => directory
+  exec { '/usr/bin/pacman --noconfirm -U http://foreman.magfest.net/bootmedia/install/pkgs/antimicro-2.6-1-x86_64.pkg.tar.xz':
+    creates => "/opt/antimicro"
   }
-  file { '/root/.config/antimicro/antimicro_settings.ini':
+  file { ['/home/challenges/.config', '/home/challenges/.config/antimicro']:
+    ensure => directory,
+    owner => challenges
+  }
+  file { '/home/challenges/.config/antimicro/antimicro_settings.ini':
     ensure => file,
     source => 'puppet:///modules/emulators/antimicro_settings.ini',
+    owner => challenges
   }
   package { 'stella':
     ensure => latest
@@ -40,12 +42,12 @@ class emulators {
     owner => challenges,
     group => challenges
   } ->
-  file { '/home/challenges/.profile':
-    source => 'puppet:///modules/emulators/profile',
-    owner => challenges,
-    group => challenges,
-    mode => 755
-  }
+#  file { '/home/challenges/.profile':
+#    source => 'puppet:///modules/emulators/profile',
+#    owner => challenges,
+#    group => challenges,
+#    mode => 755
+#  }
   file { '/usr/bin/startchallenges':
     source => 'puppet:///modules/emulators/startchallenges',
     mode => 755
@@ -60,9 +62,27 @@ class emulators {
     enable => true,
     require => File['/etc/systemd/system/challenge.service']
   }
-  file { '/opt/challenges':
+#  file { '/opt/challenges':
+#    owner => challenges,
+#    group => challenges,
+#    recurse => true
+#  }
+  file { '/home/challenges/home.tar':
+    ensure => file,
     owner => challenges,
-    group => challenges,
-    recurse => true
-  }	
+    source => 'puppet:///modules/emulators/home.tar'
+  }
+  file { '/opt/challenges.tar':
+    ensure => file,
+    owner => challenges,
+    source => 'puppet:///modules/emulators/challenges.tar'
+  }
+  exec { '/usr/bin/tar -xvf ./home.tar':
+    cwd => '/home/challenges',
+    creates => '/home/challenges/Challenge.py'
+  }
+  exec { '/usr/bin/tar -xvf ./challenges.tar':
+    cwd => '/opt',
+    creates => '/opt/challenges/roms/SNES'
+  }
 }
